ffc73c5f02480fc7b14dd0acd849244ac3528cf8
cuongdotuan <dotuancuong911@gmail.com>
2023-10-12 22:43:39 +0700

    test

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,7 +6,7 @@ const Navigation = () => {
   const { cart } = useContext(CartContext)
   return (
     <nav className='flex justify-center gap-8 pt-3 pb-1 text-xl '>
-      <NavLink className='' to={"/"}>Home</NavLink>
+      <NavLink className='' to={"/"}>???</NavLink>
       <NavLink className='' to={"products"}>Products</NavLink>
       <NavLink className='' to={"about"}>About</NavLink>
       <NavLink className='' to={"cart"}>Cart ({cart.length})</NavLink>
